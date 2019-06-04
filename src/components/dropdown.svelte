42c71209d60ff04c822a97c7aae316ee5d218202
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-06-03 18:21:24 -0700

    feat: create dropdown component

---
+++ src/components/dropdown.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let label,
+    id,
+    options,
+    selected
+</script>
+
+<label for={id}>{label}</label>
+<select {id} bind:value={selected}>
+  {#each options as { value, label: optLabel}}
+    <option {value}>{optLabel}</option>
+  {/each}
+</select>
+
